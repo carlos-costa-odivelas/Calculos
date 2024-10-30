e872d862fa36605c5610980d782aa126eed74419
carlos-costa-odivelas <51975226+carlos-costa-odivelas@users.noreply.github.com>
2024-10-30 00:06:01 +0000

    Adicionado código para testar os métodos.

--- Program.cs
+++ Program.cs
@@ -21,6 +21,12 @@ namespace Calculos
             Console.WriteLine($"Soma = {Aritmetica.Somar(n1, n2)}\n");
             Console.WriteLine($"Diferença = {Aritmetica.Subtrair(n1, n2)}\n");
 
+            double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit\n");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius\n");
+
             Console.ReadKey();
         }
     }
